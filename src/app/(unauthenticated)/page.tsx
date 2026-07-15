72afbc66465228e3ce99d255faec31b995384624
João Vitor <j.vitor2300@gmail.com>
2026-07-15 11:24:14 -0300

    feat: popular reviews mocked component

--- src/app/(unauthenticated)/page.tsx
+++ src/app/(unauthenticated)/page.tsx
@@ -1,12 +1,14 @@
 import { Hero } from "./(home)/_components/hero";
 import { PopularGames } from "./(home)/_components/popular-games";
+import { PopularReviews } from "./(home)/_components/popular-reviews";
 
 export default function Page() {
   return (
-    <div className="w-full h-full flex flex-col justify-center">
+    <div className="w-full flex flex-col justify-center">
       <Hero />
       <div className="container mx-auto">
         <PopularGames />
+        <PopularReviews />
       </div>
     </div>
   );
